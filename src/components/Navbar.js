15cb625e27c67515e96c54558eafff45734c22e7
Alex Curran <alexcurran.va@gmail.com>
2020-10-30 10:39:14 -0500

    Added relative path for homepage deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,31 @@ import React, { useState, useEffect, useRef } from 'react';
 import { links } from "../data";
 
 function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+        if (showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`;
+        } else {
+            linksContainerRef.current.style.height = "0px";
+        }
+    }, [showLinks]);
+    
     return (
         <nav>
             <div className="nav-center">
                 <div className="nav-header">
                     <h1>Logo</h1>
-                    <button className="nav-toggle">
+                    <button onClick={() => setShowLinks(!showLinks)} className="nav-toggle">
                         <svg className="burger-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </button>
                 </div>
-                <div className="links-container show-container">
-                    <ul className="links">
+                <div className="links-container" ref={linksContainerRef}>
+                    <ul className="links" ref={linksRef}>
                         {links.map(link => (
                             <li key={link.id}>
                                 <a href={link.url}>{link.text}</a>
